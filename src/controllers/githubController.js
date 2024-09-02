eb838db7fe88370d010b6f2b8b0ff20f57fc721e
Tanishq Patidar <tanishq.patidar@kalvium.community>
2024-09-02 10:04:49 +0530

    chore: Add inviteUser and removeUser methods to GithubController

--- src/controllers/githubController.js
+++ src/controllers/githubController.js
@@ -1,4 +1,3 @@
-// src/controllers/githubController.js
 import githubService from '../services/githubService.js';
 
 class GithubController {
@@ -7,6 +6,29 @@ class GithubController {
       const { since, per_page } = req.query;
       const users = await githubService.listUsers(since, per_page);
       res.status(200).json(users);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async inviteUser(req, res, next) {
+    try {
+      const { email, role, team_ids } = req.body;
+      console.log(email, role, team_ids);
+      
+      const result = await githubService.inviteUserToOrg(email, role, team_ids);
+      res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async removeUser(req, res, next) {
+    try {
+      const { username } = req.params;
+      
+      const result = await githubService.removeUserFromOrg(username);
+      res.status(200).json(result);
     } catch (error) {
       next(error);
     }
